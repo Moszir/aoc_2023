6c7500f4a5b5a0cb0fa885e784778a0516739ecc
Moszir <molnarszipai.richard@gmail.com>
2023-12-07 15:00:24 +0100

    Simplify day 7

--- day_07/solution_07.py
+++ day_07/solution_07.py
@@ -1,9 +1,7 @@
 import unittest
 
-labels = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
-b_labels = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
-
-hand_types = ['five', 'four', 'full', 'three', 'two pair', 'one pair', 'nothing']
+labels = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+b_labels = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A']
 
 
 class Hand:
@@ -11,51 +9,12 @@ class Hand:
         self.hand: str = line.split(' ')[0]
         self.bid: int = int(line.split(' ')[1])
 
-        self.type = self.rank(self.hand)
-        self.type_rank = hand_types.index(self.type)
-        self.label_ranks = [labels.index(card) for card in self.hand]
+        self.type_rank = sorted([self.hand.count(label) for label in labels], reverse=True)
+        self.label_rank = [labels.index(card) for card in self.hand]
 
-        self.b_type = self.b_rank(self.hand)
-        self.b_type_rank = hand_types.index(self.b_type)
-        self.b_label_ranks = [b_labels.index(card) for card in self.hand]
-
-    @staticmethod
-    def rank(hand):
-        counts = [hand.count(label) for label in labels]
-        return (
-            'five' if 5 in counts else
-            'four' if 4 in counts else
-            'full' if 3 in counts and 2 in counts else
-            'three' if 3 in counts else
-            'two pair' if counts.count(2) == 2 else
-            'one pair' if 2 in counts else
-            'nothing')
-
-    @staticmethod
-    def b_rank(hand):
-        counts = [hand.count(label) for label in labels if label != 'J']
-        jokers = hand.count('J')
-        if jokers in (4, 5):
-            return 'five'
-        elif jokers == 3:  # JJJaa or JJJab
-            return 'five' if 2 in counts else 'four'
-        elif jokers == 2:  # JJaaa or JJaab or JJabc
-            return 'five' if 3 in counts else 'four' if 2 in counts else 'three'
-        elif jokers == 1:
-            # Jaaaa Jaaab Jaabb Jaabc Jabcd
-            if 4 in counts:
-                return 'five'
-            elif 3 in counts:
-                return 'four'
-            elif 2 in counts and counts.count(2) == 2:
-                return 'full'
-            elif 2 in counts:
-                return 'three'
-            else:
-                return 'one pair'
-        else:
-            # No jokers: rank normally
-            return Hand.rank(hand)
+        self.type_rank_b = sorted([self.hand.count(label) for label in labels if label != 'J'], reverse=True)
+        self.type_rank_b[0] += self.hand.count('J')
+        self.label_rank_b = [b_labels.index(card) for card in self.hand]
 
 
 class Solution:
@@ -65,17 +24,13 @@ class Solution:
             self.__hands = [Hand(line) for line in lines]
 
     def solve_a(self) -> int:
-        # Sort so that the best hand is at the end
-        # The labels and hand types are listed best-to-worst, so sorting based on their indexes should be reversed.
-        hands = sorted(self.__hands, key=lambda hand: (hand.type_rank, hand.label_ranks), reverse=True)
+        hands = sorted(self.__hands, key=lambda hand: (hand.type_rank, hand.label_rank))
         return sum(
             (idx+1) * hand.bid
             for idx, hand in enumerate(hands))
 
     def solve_b(self) -> int:
-        # Sort so that the best hand is at the end
-        # The labels and hand types are listed best-to-worst, so sorting based on their indexes should be reversed.
-        hands = sorted(self.__hands, key=lambda hand: (hand.b_type_rank, hand.b_label_ranks), reverse=True)
+        hands = sorted(self.__hands, key=lambda hand: (hand.type_rank_b, hand.label_rank_b))
         return sum(
             (idx+1) * hand.bid
             for idx, hand in enumerate(hands))
