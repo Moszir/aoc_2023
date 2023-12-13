9a71e72db03c22a502cfe8d436477a9e518eba77
Moszir <molnarszipai.richard@gmail.com>
2023-12-13 06:57:01 +0100

    Clean up day 13

--- day_13/solution.py
+++ day_13/solution.py
@@ -1,100 +1,55 @@
-import copy
-import dataclasses
-import itertools
-import math
 import pathlib
 import typing
 import unittest
 
-from moszir_utils.asterisk import *
+type Problem = typing.List[typing.List[str]]
+type MaybeInt = typing.Optional[int]
 
 
 class Solution:
     def __init__(self, path: pathlib.Path):
-        self.problems = path.read_text().split('\n\n')
+        self.problems = [
+            [[c for c in row] for row in problem.split('\n')]
+            for problem in path.read_text().split('\n\n')]
 
-    def find_horizontal(self, rows: typing.List[str]) -> typing.Optional[int]:
+    @staticmethod
+    def find_horizontal(rows: Problem, excluded: MaybeInt = None) -> MaybeInt:
         for i in range(len(rows)-1):
             match = True
             for j in range(i+1):
                 opposite = (2*(i+1)-(j+1))
-                if 0 <= opposite < len(rows) and rows[j] != rows[(2*(i+1)-(j+1))]:
+                if 0 <= opposite < len(rows) and rows[j] != rows[opposite]:
                     match = False
                     break
-            if match:
-                # print('found mirror ', i+1)
+            if match and i+1 != excluded:
                 return i+1
         return None
 
-    def find_horizontal_b(self, rows: typing.List[typing.List[str]]) -> typing.List[int]:
-        results = []
-        for i in range(len(rows)-1):
-            match = True
-            for j in range(i+1):
-                opposite = (2*(i+1)-(j+1))
-                if 0 <= opposite < len(rows) and rows[j] != rows[(2*(i+1)-(j+1))]:
-                    match = False
-                    break
-            if match:
-                results.append(i+1)
-                # print('found mirror ', i+1)
-        return results
-
-    def find_mirror(self, rows: typing.List[typing.List[str]]) -> int:
-        # rows = problem.split('\n')
-        # print(rows)
-        r = self.find_horizontal(rows)
+    def find_mirror(self, rows: Problem, excluded: MaybeInt = None) -> MaybeInt:
+        excluded_row = excluded // 100 if excluded is not None and excluded % 100 == 0 else None
+        r = self.find_horizontal(rows, excluded=excluded_row)
         if r is not None:
-            # print('find_mirror ', 100*r)
-            return 100*r
-        return self.find_horizontal([''.join(col) for col in zip(*rows)])
-        # # print([''.join(col) for col in zip(*rows)])
-
-    def find_mirror_b(self, rows: typing.List[typing.List[str]], excluded_value) -> typing.Optional[int]:
-        # rows = problem.split('\n')
-        # print(rows)
-        r = self.find_horizontal_b(rows)
-        for i in r:
-            if 100*i != excluded_value:
-                return 100*i
-        r = self.find_horizontal_b([col for col in zip(*rows)])
-        for i in r:
-            if i != excluded_value:
-                return i
-        return None
-        # # print([''.join(col) for col in zip(*rows)])
+            return r*100
+        return self.find_horizontal([col for col in zip(*rows)], excluded=excluded)
 
     def solve_a(self) -> int:
         return sum((self.find_mirror(problem) for problem in self.problems))
 
-    def solve_one_b(self, rows: typing.List[typing.List[str]]) -> int:
+    def solve_one_b(self, rows: Problem) -> int:
+        """ Just brute force it. """
         original_solution = self.find_mirror(rows)
-        print(original_solution)
-        spots = len(rows) * len(rows[0])
         for ri in range(len(rows)):
             for ci in range(len(rows[0])):
                 original = rows[ri][ci]
                 rows[ri][ci] = '#' if original == '.' else '.'
-                result = self.find_mirror_b(rows, original_solution)
-                print(ri, ci, result)
-                if result is not None and result != original_solution:
-                    # print(ri, ci)
+                result = self.find_mirror(rows, excluded=original_solution)
+                if result is not None:
                     return result
                 rows[ri][ci] = original
-        print(rows)
         assert False
-        return -1
 
     def solve_b(self) -> int:
-        accu = 0
-        i = 0
-        for problem in self.problems:
-            print('starting ', i)
-            i += 1
-            accu += self.solve_one_b(
-                [[c for c in row] for row in problem.split('\n')])
-
-        return accu
+        return sum((self.solve_one_b(problem) for problem in self.problems))
 
 
 class Tests(unittest.TestCase):
@@ -110,13 +65,13 @@ class Tests(unittest.TestCase):
         self.assertEqual(405, self.example().solve_a())
 
     def test_a_input(self):
-        self.assertEqual(43614, self.real_input().solve_a())
+        self.assertEqual(43_614, self.real_input().solve_a())
 
     def test_b_example(self):
         self.assertEqual(400, self.example().solve_b())
 
     def test_b_input(self):
-        self.assertEqual(0, self.real_input().solve_b())
+        self.assertEqual(36_771, self.real_input().solve_b())
 
 
 if __name__ == '__main__':
