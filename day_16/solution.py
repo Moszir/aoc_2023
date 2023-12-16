219c7b12b63b2e46c7ab558c04ae51a1ad3e896c
Moszir <molnarszipai.richard@gmail.com>
2023-12-16 07:08:43 +0100

    clean up day 16

--- day_16/solution.py
+++ day_16/solution.py
@@ -1,12 +1,8 @@
 import copy
 import dataclasses
-import itertools
-import math
 import pathlib
 import typing
 import unittest
-
-from moszir_utils.asterisk import *
 
 
 class Solution:
@@ -14,110 +10,84 @@ class Solution:
         self.lines = path.read_text().splitlines()
         self.height = len(self.lines)
         self.width = len(self.lines[0])
-        # print(self.lines)
 
     @dataclasses.dataclass
     class Beam:
         row: int
         col: int
         direction: str
-        
-        def next(self):
-            if self.direction == 'right':
-                self.col += 1
-            if self.direction == 'left':
-                self.col -= 1
-            if self.direction == 'down':
-                self.row += 1
-            if self.direction == 'up':
-                self.row -= 1
 
         def as_tuple(self):
             return self.row, self.col, self.direction
 
-    def solve_a(self, starting_beam=(0, 0, 'right')) -> int:
-        energized = [[0 for c in line] for line in self.lines]
-        beams = [Solution.Beam(*starting_beam)]
+        def next(self) -> 'Solution.Beam':
+            if self.direction == 'right':
+                self.col += 1
+            elif self.direction == 'left':
+                self.col -= 1
+            elif self.direction == 'down':
+                self.row += 1
+            elif self.direction == 'up':
+                self.row -= 1
+            return self
+
+        _backslash = {
+            'right': 'down',
+            'down': 'right',
+            'left': 'up',
+            'up': 'left',
+        }
+
+        def hit_backslash(self) -> 'Solution.Beam':
+            self.direction = self._backslash[self.direction]
+            return self
+
+        _slash = {
+            'right': 'up',
+            'up': 'right',
+            'left': 'down',
+            'down': 'left',
+        }
+
+        def hit_slash(self) -> 'Solution.Beam':
+            self.direction = self._slash[self.direction]
+            return self
+
+        def react(self, spot: str) -> typing.List['Solution.Beam']:
+            if spot == '\\':
+                return [self.hit_backslash().next()]
+            elif spot == '/':
+                return [self.hit_slash().next()]
+            elif (spot == '|' and self.direction in ('left', 'right')) or (spot == '-' and self.direction in ('up', 'down')):
+                twin = copy.copy(self)
+                return [self.hit_slash().next(), twin.hit_backslash().next()]
+            return [self.next()]
+
+    def count_energized_beams(self, starting_beam: Beam) -> int:
+        energized = [[False for _ in line] for line in self.lines]
+        beams = [starting_beam]
         seen = set()
         while len(beams) > 0:
-            beam = beams.pop(0)
-            # print(len(beams), beam)
-            if beam.as_tuple() in seen:
+            beam = beams.pop()
+            if not (0 <= beam.row < self.height and 0 <= beam.col < self.width) or beam.as_tuple() in seen:
                 continue
             seen.add(beam.as_tuple())
-            if 0 <= beam.row < self.height and 0 <= beam.col < self.width:
-                energized[beam.row][beam.col] += 1
-                s = self.lines[beam.row][beam.col]
-                if s == '.':
-                    beam.next()
-                    beams.append(beam)
-                if s == '\\':
-                    if beam.direction == 'right':
-                        beam.direction = 'down'
-                    elif beam.direction == 'left':
-                        beam.direction = 'up'
-                    elif beam.direction == 'down':
-                        beam.direction = 'right'
-                    elif beam.direction == 'up':
-                        beam.direction = 'left'
-                    beam.next()
-                    beams.append(beam)
-                elif s == '/':
-                    if beam.direction == 'right':
-                        beam.direction = 'up'
-                    elif beam.direction == 'left':
-                        beam.direction = 'down'
-                    elif beam.direction == 'down':
-                        beam.direction = 'left'
-                    elif beam.direction == 'up':
-                        beam.direction = 'right'
-                    beam.next()
-                    beams.append(beam)
-                elif s == '|':
-                    if beam.direction in ('up', 'down'):
-                        beam.next()
-                        beams.append(beam)
-                    else:
-                        beam2 = copy.copy(beam)
-                        beam.direction = 'up'
-                        beam.next()
-                        beams.append(beam)
-                        beam2.direction = 'down'
-                        beam2.next()
-                        beams.append(beam2)
-                elif s == '-':
-                    if beam.direction in ('left', 'right'):
-                        beam.next()
-                        beams.append(beam)
-                    else:
-                        beam2 = copy.copy(beam)
-                        beam.direction = 'left'
-                        beam.next()
-                        beams.append(beam)
-                        beam2.direction = 'right'
-                        beam2.next()
-                        beams.append(beam2)
+            energized[beam.row][beam.col] = True
+            beams.extend(beam.react(self.lines[beam.row][beam.col]))
+        return sum((1 for line in energized for spot in line if spot))
 
-        return sum((1 for line in energized for c in line if c > 0))
+    def solve_a(self) -> int:
+        return self.count_energized_beams(Solution.Beam(0, 0, 'right'))
 
     def solve_b(self) -> int:
-        m = 0
-        for col in range(self.width):
-            c = self.solve_a((0, col, 'down'))
-            if c > m:
-                m = c
-            c = self.solve_a((self.height-1, col, 'up'))
-            if c > m:
-                m = c
-        for row in range(self.height):
-            c = self.solve_a((row, 0, 'right'))
-            if c > m:
-                m = c
-            c = self.solve_a((row, self.width-1, 'left'))
-            if c > m:
-                m = c
-
-        return m
+        return max((
+            self.count_energized_beams(beam)
+            for beam in (
+                *(Solution.Beam(0, col, 'down') for col in range(self.width)),
+                *(Solution.Beam(self.height-1, col, 'up') for col in range(self.width)),
+                *(Solution.Beam(row, 0, 'right') for row in range(self.width)),
+                *(Solution.Beam(row, self.width-1, 'left') for row in range(self.width))
+            )))
 
 
 class Tests(unittest.TestCase):
@@ -139,7 +109,7 @@ class Tests(unittest.TestCase):
         self.assertEqual(51, self.example().solve_b())
 
     def test_b_input(self):
-        self.assertEqual(0, self.real_input().solve_b())
+        self.assertEqual(7_594, self.real_input().solve_b())  # 2.6s, good enough
 
 
 if __name__ == '__main__':
