6c1553bacbfe0c46cefd7e5c469911edee51b5c5
Moszir <molnarszipai.richard@gmail.com>
2023-12-20 14:04:44 +0100

    template change

--- template/readme.md
+++
@@ -1,9 +0,0 @@
-# Solution Template
-
-1. Copy the example input into `example.txt`
-2. Copy the actual input into `input.txt`
-3. Implement input reading logic in `Solution.__init__`
-4. Adjust the expected output for the example in `Tests.test_a_example`
-5. Implement solution to the first problem in `Solution.solve_a`, until `test_a_example` passes.
-6. Run `Tests.test_a_input` to get the solution to the first problem.
-7. Repeat 4-6 for the second problem. 
--- template/solution.py
+++ template/solution.py
@@ -1,48 +1,11 @@
-import copy
-import dataclasses
-import itertools
+from collections import defaultdict, deque
 import math
-import pathlib
-import typing
-import unittest
-
-from moszir_utils.asterisk import *
 
 
-class Solution:
-    def __init__(self, path: pathlib.Path):
-        self.lines = path.read_text().splitlines()
-        self.height = len(self.lines)
-        self.width = len(self.lines[0])
-
-    def solve_a(self) -> int:
-        return 0
-
-    def solve_b(self) -> int:
-        return 0
+file_path = 'example.txt'
+# file_path = 'input.txt'
+with open(file_path) as f:
+    lines = f.read().strip().split('\n')
 
 
-class Tests(unittest.TestCase):
-    @staticmethod
-    def example():
-        return Solution(pathlib.Path('example.txt'))
 
-    @staticmethod
-    def real_input():
-        return Solution(pathlib.Path('input.txt'))
-
-    def test_a_example(self):
-        self.assertEqual(0, self.example().solve_a())
-
-    def test_a_input(self):
-        self.assertEqual(0, self.real_input().solve_a())
-
-    def test_b_example(self):
-        self.assertEqual(0, self.example().solve_b())
-
-    def test_b_input(self):
-        self.assertEqual(0, self.real_input().solve_b())
-
-
-if __name__ == '__main__':
-    unittest.main()
