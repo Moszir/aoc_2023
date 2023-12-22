e6f2168b6565d54ac696cfc2bf2b78b0d9bc5025
Moszir <molnarszipai.richard@gmail.com>
2023-12-22 08:27:40 +0100

    Clean up day 22

--- day_22/solution.py
+++ day_22/solution.py
@@ -1,95 +1,74 @@
-from collections import defaultdict, deque
-import math
+real_input = True
+file_path = 'input.txt' if real_input else 'example.txt'
+expected_solution_1 = 446 if real_input else 5
+expected_solution_2 = 60287 if real_input else 7
 
+bricks = [
+    [
+        int(x)
+        for part in line.split('~')
+        for x in part.split(',')
+    ]
+    for line in open(file_path).readlines()
+]
 
-# file_path = 'example.txt'
-file_path = 'input.txt'
-with open(file_path) as f:
-    lines = f.read().strip().split('\n')
-
-bricks = []
-for line in lines:
-    p = line.strip().split('~')[0]
-    q = line.strip().split('~')[1]
-    bricks.append([int(p.split(',')[0]), int(p.split(',')[1]), int(p.split(',')[2]), int(q.split(',')[0]), int(q.split(',')[1]), int(q.split(',')[2])])
-
-# print(bricks)
-
+# This way the falling can go with a single for loop.
 bricks = sorted(bricks, key=lambda b: b[2])
+n = len(bricks)
 
-
-def proj(brick):
-    return [
+xy_projections = [
+    [
         (x, y)
         for x in range(brick[0], brick[3]+1)
-        for y in range(brick[1], brick[4]+1)]
+        for y in range(brick[1], brick[4]+1)
+    ]
+    for brick in bricks
+]
 
 
-change_happened = True
-while change_happened:
-    change_happened = False
-    for i, brick in enumerate(bricks):
-        z = brick[2]
-        min_z = 1
-        my_proj = proj(brick)
-        for j, b2 in enumerate(bricks):
-            if j == i:
-                continue
-            if b2[2] >= z or b2[5] < min_z:
-                continue
-            b2_proj = proj(b2)
-            if any((p in b2_proj for p in my_proj)):
-                min_z = b2[5]+1
-                # print(brick, my_proj, 'is blocked by', b2, b2_proj)
-        if min_z < z:
-            fall = z-min_z
-            brick[2] = min_z
-            brick[5] -= fall
-            change_happened = True
-# 5
+def intersects(i1, i2):
+    return any((p in xy_projections[i2] for p in xy_projections[i1]))
 
-# print(bricks)
+
+for i, brick in enumerate(bricks):
+    min_z = 1  # Find the largest z, that intersects with the projection of the brick
+    for j in range(i):
+        if bricks[j][5] >= min_z and intersects(i, j):
+            min_z = bricks[j][5]+1
+    brick[5] -= brick[2]-min_z
+    brick[2] = min_z
 
 supported_by = [[] for b in bricks]
 
-for i, b in enumerate(bricks):
-    b_proj = proj(b)
-    for j, b2 in enumerate(bricks):
-        if i == j:
-            continue
-        if b2[5]+1 != b[2]:
-            continue
-        b2_proj = proj(b2)
-        if any((p in b2_proj for p in b_proj)):
+for i in range(n):
+    # The bricks are no longer necessarily sorted by z, but a supporter could not have fallen below what it supports.
+    for j in range(i):
+        if bricks[j][5]+1 == bricks[i][2] and intersects(i, j):
             supported_by[i].append(j)
 
-# print(supported_by)
-# eliminateable: no [i] exists
-print(
-    sum((
-        1
-        for i in range(len(bricks))
-        if [i] not in supported_by)))
+# Brick `i` cannot be eliminated, if there is a brick that is only supported by `i`.
+solution_1 = sum((1 for i in range(len(bricks)) if [i] not in supported_by))
+print(solution_1)
+if solution_1 != expected_solution_1:
+    print("This doesn't seem right: expected", expected_solution_1, "got", solution_1)
 
+# -------------------------------------------------------------------------------------------------
+# Part 2
 
-accu = 0
-for i in range(len(bricks)):
-    falls = 0
+# assert all((all((i > j for j in s)) for i, s in enumerate(supported_by)))
+# Make those on the ground be supported by '-1'
+for s in supported_by:
+    if not s:
+        s.append(-1)
+
+solution_2 = 0
+for i in range(n):
     removed = [i]
-    change = True
-    # print(i)
-    while change:
-        change = False
-        for j, s in enumerate(supported_by):
-            if j in removed or s == []:
-                continue
-            # print('  ', j)
-            if all((k in removed for k in s)):
-                # print('  all of ', s, 'in', removed)
-                falls += 1
-                removed.append(j)
-                change = True
-    accu += len(removed)-1  # -self
-    # print("Removing ", i, removed)
+    for j in range(i+1, n):
+        if all((k in removed for k in supported_by[j])):
+            removed.append(j)
+    solution_2 += len(removed) - 1  # -1: brick `i` itself
 
-print(accu)
+print(solution_2)
+if solution_2 != expected_solution_2:
+    print("This doesn't seem right: expected", expected_solution_2, "got", solution_2)
